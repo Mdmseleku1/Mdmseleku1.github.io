7672f637290e36eb7d59086d7107c385fc9b9613
tmanmoseri <45037934+tmanmoseri@users.noreply.github.com>
2020-02-05 09:20:05 +0200

    Add files via upload

--- classes/GenerateReport.php
+++ classes/GenerateReport.php
@@ -1,18 +1,20 @@
 <?php
 
 class GenerateReport{
-    
+
     public static function getTaxReportData(){
-        
+
         $empID = Login::isLoggedIn();
-        
+
         $compliedItems = DB::query('SELECT tax.sNo, tax.description, tax.country, tax.ou, tax.entity, tax.period, tax.fy, tax.dueFor, tax.dueIn, tax.empID, tax.compliedOn, users.empID FROM tax, users
         WHERE users.empID = tax.empID ORDER BY tax.sNo DESC;', array(':empID'=>$empID));
-        
+
         foreach($compliedItems as $comp) {
-            
+
             $class = "alert-success";
-            
+            //  $class1 = "alert-warning";
+                $class2 = "alert-danger";
+
             echo "<tr>
                 <td>".$comp['sNo']."</td>
                 <td>".$comp['country']."</td>
@@ -23,13 +25,19 @@ class GenerateReport{
                 <td>".$comp['period']."</td>
 	            <td>".$comp['dueFor']."</td>
 	            <td>".$comp['dueIn']."</td>
-                <td class='alert ".$class."'>".$comp['compliedOn']."</td>
+                <td if(.$comp['compliedOn']. <= .$comp['dueIn']. ){
+                  class='alert ".$class."'
+                }
+                else{
+                  class='alert ".$class2."'
+                }
+                >".$comp['compliedOn']."</td>
                 </tr>";
         }
-    
+
     }
-    
+
 }
-    
-    
+
+
 ?>
