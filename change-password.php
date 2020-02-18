f9ba089ed2f77d6641d5f43587832f03514d993a
Mdmseleku1 <mdmseleku95@gmail.com>
2020-02-18 09:29:48 +0200

    Update change-password.php

--- change-password.php
+++ change-password.php
@@ -12,7 +12,7 @@ if (Login::isLoggedIn()) {
             
     $url = "https://www.google.com/recaptcha/api/siteverify";
     $data = [
-			'secret' => "6LexmdkUAAAAAFdVXetRc1WiS5D5RIODM8cVkrXS",
+			'secret' => "your_secret_key",
 			'response' => $_POST['token'],
 			'remoteip' => $_SERVER['REMOTE_ADDR']
 		];
@@ -71,7 +71,7 @@ if (Login::isLoggedIn()) {
                     
     $url = "https://www.google.com/recaptcha/api/siteverify";
     $data = [
-			'secret' => "6LexmdkUAAAAAFdVXetRc1WiS5D5RIODM8cVkrXS",
+			'secret' => "your_secret_key",
 			'response' => $_POST['token'],
 			'remoteip' => $_SERVER['REMOTE_ADDR']
 		];
@@ -140,7 +140,7 @@ if (Login::isLoggedIn()) {
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/styles.min.css">
-    <script src="https://www.google.com/recaptcha/api.js?render=6LexmdkUAAAAAMRZ8X2k7cFIt7MiUA1zqEPYiVmg"></script>
+    <script src="https://www.google.com/recaptcha/api.js?render=your_site_key"></script>
 </head>
 
 <body class="bg-gradient-primary" style="background-color: #092c6e;background-image: url(&quot;none&quot;);">
@@ -192,7 +192,7 @@ if (Login::isLoggedIn()) {
     <script src="assets/js/script.min.js"></script>
     <script>
           grecaptcha.ready(function() {
-              grecaptcha.execute('6LexmdkUAAAAAMRZ8X2k7cFIt7MiUA1zqEPYiVmg', {action: 'homepage'}).then(function(token) {
+              grecaptcha.execute('your_secret_key', {action: 'homepage'}).then(function(token) {
                  // console.log(token);
                  document.getElementById("token").value = token;
               });
