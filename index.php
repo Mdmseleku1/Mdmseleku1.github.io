5944a10dfec75d1ad0b9e5c85a1146c1e00924e8
Mdmseleku1 <mdmseleku95@gmail.com>
2020-02-18 09:27:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if(isset($_POST['submit'])){
     
     $url = "https://www.google.com/recaptcha/api/siteverify";
     $data = [
-			'secret' => "6LexmdkUAAAAAFdVXetRc1WiS5D5RIODM8cVkrXS",
+			'secret' => "your_secret_key",
 			'response' => $_POST['token'],
 			'remoteip' => $_SERVER['REMOTE_ADDR']
 		];
@@ -135,7 +135,7 @@ if(isset($_POST['submit'])){
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/styles.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
-    <script src="https://www.google.com/recaptcha/api.js?render=6LexmdkUAAAAAMRZ8X2k7cFIt7MiUA1zqEPYiVmg"></script>
+    <script src="https://www.google.com/recaptcha/api.js?render="your_site_key"></script>
         
     <!-- SELECT CSS TO IMPLEMENT SEARCH WITH OPTIONS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
@@ -684,7 +684,7 @@ function toggle_visibility() {
 </script>
 <script>
           grecaptcha.ready(function() {
-              grecaptcha.execute('6LexmdkUAAAAAMRZ8X2k7cFIt7MiUA1zqEPYiVmg', {action: 'homepage'}).then(function(token) {
+              grecaptcha.execute('your_site_key', {action: 'homepage'}).then(function(token) {
                  // console.log(token);
                  document.getElementById("token").value = token;
               });
